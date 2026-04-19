494d17352871e05e8aa9af92f19d3de74a7b69e0
Jaloliddin Gulimov <gulimovjalol1999-bit>
2026-04-19 23:32:26 +0500

    lesson2

---
+++ src/article/article.service.ts
@@ -0,0 +1,37 @@
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import { CreateArticleDto } from './dto/create-article.dto';
+import { UpdateArticleDto } from './dto/update-article.dto';
+import { InjectModel } from '@nestjs/sequelize';
+import { Article } from './model/article.entity';
+
+@Injectable()
+export class ArticleService {
+  constructor(@InjectModel(Article) private articleModel: typeof Article) {}
+  async create(createArticleDto: CreateArticleDto) {
+    return await this.articleModel.create({...createArticleDto});
+  }
+
+  async findAll(): Promise<Article[]> {
+    return await this.articleModel.findAll();
+  }
+
+  async findOne(id: number): Promise<Article>  {
+    const foundedArticle = await this.articleModel.findByPk(id)
+    if(!foundedArticle) throw new NotFoundException("article not found")
+    return foundedArticle;
+  }
+
+  async update(id: number, updateArticleDto: UpdateArticleDto): Promise<{message: string}> {
+    const foundedArticle = await this.articleModel.findByPk(id)
+    if(!foundedArticle) throw new NotFoundException("article not found")
+    await foundedArticle.update(updateArticleDto)
+    return {message: `Updated`};
+  }
+
+  async remove(id: number): Promise<{message: string}> {
+    const foundedArticle = await this.articleModel.findByPk(id)
+    if(!foundedArticle) throw new NotFoundException("article not found")
+    await foundedArticle.destroy()
+    return {message: "Deleted"};
+  }
+}
---
+++ src/auth/model/auth.entity.ts
@@ -0,0 +1,45 @@
+import { DataTypes } from "sequelize";
+import {
+  // BeforeCreate,
+  Column,
+  HasMany,
+  Model,
+  Table,
+} from "sequelize-typescript";
+import { Article } from "src/article/model/article.entity";
+// import * as bcrypt from "bcrypt";
+
+@Table({ tableName: "auth", timestamps: true })
+export class Auth extends Model {
+  @Column({
+    type: DataTypes.STRING,
+    allowNull: false,
+  })
+  username!: string;
+
+  @Column({
+    type: DataTypes.STRING,
+    allowNull: false,
+  })
+  email!: string;
+
+  @Column({
+    type: DataTypes.STRING,
+    allowNull: false,
+  })
+  password!: string;
+
+  @Column({
+    type: DataTypes.STRING,
+    allowNull: false,
+  })
+  otp!: string;
+
+  @HasMany(() => Article)
+  articles?: Article[];
+
+  // @BeforeCreate
+  // static async hashPassword(user: Auth) {
+  //   user.password = await bcrypt.hash(user.password, 10);
+  // }
+}
